2c669ef673bedfb0dc6ed685d49e0de57f5cbfde
Ugo Pattacini <ugo.pattacini@iit.it>
2016-09-07 14:05:31 +0200

    code is almost there

--- modules/compute-trajectory.cpp
+++ modules/compute-trajectory.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015 iCub Facility - Istituto Italiano di Tecnologia
+ * Copyright (C) 2016 iCub Facility - Istituto Italiano di Tecnologia
  * Author: Ugo Pattacini
  * email:  ugo.pattacini@iit.it
  * Permission is granted to copy, distribute, and/or modify this program
@@ -16,20 +16,50 @@
 */
 
 #include <string>
-#include <set>
 #include <iostream>
 #include <iomanip>
+#include <cmath>
 #include <algorithm>
 
 #include <yarp/os/all.h>
 #include <yarp/sig/all.h>
+#include <yarp/math/Math.h>
 #include <cer_kinematics/arm.h>
-#include <cer_kinematics/head.h>
 
 using namespace std;
 using namespace yarp::os;
 using namespace yarp::sig;
+using namespace yarp::math;
 using namespace cer::kinematics;
+
+
+/****************************************************************/
+string formatOutput(ArmSolver &s, const int i, const Matrix &Hd,
+                    const Vector &q)
+{
+    Matrix H;
+    s.fkin(q,H);
+    
+    Vector ud=dcm2axis(Hd);
+    Vector u=dcm2axis(H);
+    ud*=ud[3]; ud.pop_back();
+    u*=u[3]; u.pop_back();
+
+    double e_x=norm(Hd.getCol(3).subVector(0,2)-H.getCol(3).subVector(0,2));
+    double e_u=norm(ud-u);
+
+    Property p;
+    p.put("point",i);
+
+    Bottle b;
+    b.addList().read(const_cast<Vector&>(q));
+    p.put("q",b.get(0));
+
+    p.put("e_x",e_x);
+    p.put("e_u",e_u);
+
+    return p.toString();
+}
 
 
 /****************************************************************/
@@ -38,92 +68,78 @@ int main(int argc, char *argv[])
     ResourceFinder rf;
     rf.configure(argc,argv);
 
-    set<string> avFrames=HeadParameters::getTypes();
-    // remove those going along with arm too
-    avFrames.erase("left"); avFrames.erase("right");
-
-    string types_helper("");
-    for (set<string>::iterator it=avFrames.begin(); it!=avFrames.end(); it++)
-        types_helper+=*it+"|";
-    types_helper.erase(types_helper.end()-1);
-
     if (rf.check("help"))
     {
         cout<<"Options:"<<endl;
-        cout<<"--kinematics arm|head"<<endl;
-        cout<<"--type left|right|"<<types_helper<<endl;
-        cout<<"--q \"(0.0 1.0 ... 11.0)\"|\"(0.0 1.0 ... 5.0)\""<<endl;
+        cout<<"--type left|right"<<endl;
+        cout<<"--R1-initial-joints \"(0.0 1.0 ... 11.0)\""<<endl;
+        cout<<"--handle-length <double> [m]"<<endl;
+        cout<<"--rotation <double> [deg]"<<endl;
+        cout<<"--points <int>"<<endl;
         return 0;
     }
 
-    string kinematics=rf.check("kinematics",Value("arm")).asString();
     string type=rf.check("type",Value("left")).asString();
-    
-    transform(kinematics.begin(),kinematics.end(),kinematics.begin(),::tolower);
     transform(type.begin(),type.end(),type.begin(),::tolower);
 
-    if ((kinematics!="arm") && (kinematics!="head"))
+    if ((type!="left") && (type!="right"))
     {
-        cerr<<"unrecognized kinematics \""<<kinematics<<"\""<<endl;
+        cerr<<"unrecognized type \""<<type<<"\""<<endl;
         return 1;
     }
 
-    if ((avFrames.find(type)==avFrames.end()) &&
-        (type!="left") && (type!="right"))
-    {
-        cerr<<"unrecognized type \""<<type<<"\""<<endl;
-        return 2;
-    }
+    double handle_length=rf.check("handle-length",Value(0.08)).asDouble();
+    double rotation=rf.check("rotation",Value(10.0)).asDouble();
+    int points=rf.check("points",Value(1)).asInt();
 
-    if ((kinematics=="arm") && (type!="left") && (type!="right"))
+    Vector q0(12);
+    //default torso tripod
+    q0[0]=q0[1]=q0[2]=0.1343;
+    // default torso yaw
+    q0[3]=0.0;
+    // default arm
+    q0[4]=50.509; q0[5]=23.225; q0[6]=-10.107; q0[7]=54.849; q0[8]=-62.853;
+    // default wrist tripod
+    q0[9]=0.022686; q0[10]=0.020822; q0[11]=0.024994;
+    if (Bottle *b=rf.find("R1-initial-joints").asList())
     {
-        cerr<<"\""<<type<<"\" type is not allowed for arm"<<endl;
-        return 3;
-    }
-
-    Solver *solver;
-    Vector q;
-
-    if (kinematics=="arm")
-    {
-        ArmParameters armp(type);
-        solver=new ArmSolver(armp);
-        q.resize(12,0.0);
-    }
-    else
-    {
-        HeadParameters headp(type);
-        solver=new HeadSolver(headp);
-        q.resize(6,0.0);
-    }
-        
-    if (Bottle *b=rf.find("q").asList())
-    {
-        size_t len=std::min(q.length(),(size_t)b->size());
+        size_t len=std::min(q0.length(),(size_t)b->size());
         for (size_t i=0; i<len; i++)
-            q[i]=b->get(i).asDouble();
+            q0[i]=b->get(i).asDouble();
     }
 
-    Matrix H;
-    if (solver->fkin(q,H))
+    ArmParameters armp(type);
+    ArmSolver solver(armp);
+
+    SolverParameters p=solver.getSolverParameters();
+    p.setMode("full_pose+no_torso_heave");
+    p.max_cpu_time=1.0;
+    solver.setSolverParameters(p);
+
+    Matrix H0;
+    solver.fkin(q0,H0);
+    
+    Matrix C0=eye(4,4);
+    C0.setCol(3,H0.getCol(3));
+    C0(1,3)+=handle_length;
+
+    cout<<formatOutput(solver,0,H0,q0)<<endl;
+
+    H0=SE3inv(C0)*H0;
+    Vector axis=yarp::math::sign(handle_length)*C0.getCol(0);    
+
+    Vector q=q0;
+    for (int i=1; i<=points; i++)
     {
-        cout<<kinematics<<"=";
-        if (kinematics=="arm")
-            cout<<dynamic_cast<ArmSolver*>(solver)->getArmParameters().upper_arm.getType();
-        else
-            cout<<dynamic_cast<HeadSolver*>(solver)->getHeadParameters().head.getType();
-        cout<<endl;
-        cout<<"q=("<<q.toString(5,5)<<")"<<endl;
-        cout<<"H="<<H.toString(5,5)<<endl;
-        cout<<endl;
-    }
-    else
-    {
-        cerr<<"something unexpected went wrong :("<<endl;
-        return 4;
+        axis[3]=(M_PI/180.0)*(i*(rotation/points));
+        Matrix Hd=C0*axis2dcm(axis)*H0;
+
+        solver.setInitialGuess(q);
+        solver.ikin(Hd,q);
+
+        cout<<formatOutput(solver,i,Hd,q)<<endl;
     }
 
-    delete solver;
     return 0;
 }
 
